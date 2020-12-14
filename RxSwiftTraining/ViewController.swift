bdbda19e65b98667d3c0c2b27ce68380a4da459d
hayazaki <toru.hayazaki@gmail.com>
2020-12-14 10:08:22 +0900

    subscribeがネストしないようにする実装の確認

--- RxSwiftTraining/ViewController.swift
+++ RxSwiftTraining/ViewController.swift
@@ -17,6 +17,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var asyncButton: UIButton!
+    
+    @IBAction func asyncButtonTapped(_ sender: Any) {
+        print("asyncButtonTapped")
+        
+        asyncMethod2()
+            .flatMap({ obj -> Single<Int> in
+                print("main flatMap1 \(obj)")
+                return .just(1)
+            })
+            .flatMap({ [unowned self] obj -> Single<String> in
+                print("main flatMap2 \(obj)")
+                return self.asyncMethod1()
+            })
+            .subscribe(onNext: { obj in
+                print("main subscribe \(obj)")
+            })
+            .disposed(by: self.disposeBag)
+            
+        
+        print("asyncButtonTapped end")
+    }
+    
+    private func asyncMethod1() -> Single<String> {
+        print("asyncMethod1")
+        if countLabel.text == "0" {
+            return .error(NSError(domain: "asyncMethod1", code: 1, userInfo: nil))
+        }
+        return .just(countLabel.text!)
+    }
+    
+    private func asyncMethod2() -> Observable<String> {
+        print("asyncMethod2")
+        return Observable.create({ observer in
+            print("asyncMethod2 observable")
+            sleep(1)
+            observer.onNext("val1")
+            sleep(1)
+            observer.onNext("val2")
+            return Disposables.create()
+        })
+    }
+    
     
     private var count = 0
     
